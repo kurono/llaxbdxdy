1e2c00c0c923b2a11dff2657a25309b8256bce21
Kei <ven7.noir@gmail.com>
2017-11-17 14:27:04 +0300

    The Matrix class added

--- test_initial_commit.js
+++ test_initial_commit.js
@@ -1,2 +1,22 @@
-var mx = new Matrix();
-console.log(1);
+//var mx = new Matrix();
+//console.log(1);
+
+var Matrix = ( function() {
+    var _privateVar1 = "_privateVar1";
+    
+    function Matrix() {
+    };
+    
+    // private method
+    privateFun1() {
+        return "privateFun1";
+    }
+    
+    // public method
+    Matrix.prototype.get_privateVar1 = function() {
+        return _privateVar1;
+    };
+    
+    return Matrix;
+})();
+
